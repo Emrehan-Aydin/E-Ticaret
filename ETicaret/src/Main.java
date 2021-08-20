77b19c31f685fc989e81d158c086eb1ffa92b26f
Emrehan AYDIN <78824631+Emrehan-Aydin@users.noreply.github.com>
2021-08-20 14:16:36 +0300

    test

--- ETicaret/src/Main.java
+++ ETicaret/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		AccountManager accountManager = new AccountManager(
 				new HibernateAccountDao(), new GoogleServiceAdapter());
-		Account account = new Account(2,"Onur","GOZ","abcD@gmail.com","1234566");
+		Account account = new Account(3,"Onur","GOZ","abcD@gmail.com","1234566");
 		accountManager.add(account);
 
 	}
